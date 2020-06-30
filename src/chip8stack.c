e8f348997de9008827b3699f3361f4cc0c19ad6b
shreyas42 <Shreyasanupindi356@gmail.com>
2020-06-30 11:47:40 +0530

    Fixed bug in stack

--- src/chip8stack.c
+++ src/chip8stack.c
@@ -2,14 +2,14 @@
 #include "chip8stack.h"
 #include "chip8.h"
 
-static void chip8_is_stack_in_bounds(struct chip8 *chip8)
+static void chip8_stack_in_bounds(struct chip8 *chip8)
 {
     assert(chip8->registers.SP < CHIP8_MAX_STACK_DEPTH);
 }
 
 void chip8_stack_push(struct chip8 *chip8, unsigned short val)
 {
-    chip8_is_stack_in_bounds(chip8);
+    chip8_stack_in_bounds(chip8);
     chip8->stack.stack[chip8->registers.SP] = val;
     chip8->registers.SP += 1;
 }
@@ -17,8 +17,8 @@ void chip8_stack_push(struct chip8 *chip8, unsigned short val)
 unsigned short chip8_stack_pop(struct chip8 *chip8)
 {
     unsigned short result;
-    chip8_is_stack_in_bounds(chip8);
-    result = chip8->stack.stack[chip8->registers.SP];
     chip8->registers.SP -= 1;
+    chip8_stack_in_bounds(chip8);
+    result = chip8->stack.stack[chip8->registers.SP];
     return result;
 }
